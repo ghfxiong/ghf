eaf784e4eb38a215746780447cc922c56ac585cc
ghfxiong <woshixiongxinyu@163.com>
2015-09-02 09:52:47 +0800

    dd

--- src/com/iamghf/web/beans/blog/BTag.java
+++ src/com/iamghf/web/beans/blog/BTag.java
@@ -3,6 +3,14 @@ package com.iamghf.web.beans.blog;
 public class BTag {
 	private long id;
 	private String name;
+	private String url;
+	
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
 	public long getId() {
 		return id;
 	}
